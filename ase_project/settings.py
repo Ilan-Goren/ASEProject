2cade97f238710066042e58ec34497828629592f
Ahmed Aman <a.aman@sussex.ac.uk>
2024-10-11 17:48:09 +0100

    Fix: settings.py

--- ase_project/settings.py
+++ ase_project/settings.py
@@ -122,6 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS=[
-    BASE_DIR/"static",
-]
