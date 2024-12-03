76b5e7146b2f543088b762cc7ce89219f6bff065
cw523-01 <78119212+cw523-01@users.noreply.github.com>
2024-12-03 21:19:56 +0000

    improved test_solver.py

--- polyhex/tests/test_solver.py
+++ polyhex/tests/test_solver.py
@@ -1,61 +1,79 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# from ..solver_functions import solver, board, piece, algorithm_x_functions
+from ..solver_functions import solver, board, piece, algorithm_x_functions
 
-# class HexSolverTestCase(TestCase):
-#     def test_generate_board_cell_indexes(self):
-#         s = solver.Solver()
-#         b = board.Board()
-#         s.generate_board_cell_indexes(b.board)
-#         print(b.board)
-#         print(s.index_to_cell)
-#         print(s.cell_to_index)
+class HexSolverTestCase(TestCase):
+    def test_generate_board_cell_indexes(self):
+        s = solver.Solver()
+        b = board.Board()
+        s.generate_board_cell_indexes(b.board)
+        expected_board = [[[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0], [0, 0], [0]], [[0, 0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0], [0, 0], [0]], [[0, 0, 0, 0], [0, 0, 0], [0, 0], [0]], [[0, 0, 0], [0, 0], [0]], [[0, 0], [0]], [[0]]]
+        expected_index_to_cell = {1: (0, 0, 0), 2: (1, 0, 0), 3: (2, 0, 0), 4: (3, 0, 0), 5: (4, 0, 0), 6: (5, 0, 0), 7: (0, 1, 0), 8: (1, 1, 0), 9: (2, 1, 0), 10: (3, 1, 0), 11: (4, 1, 0), 12: (0, 2, 0), 13: (1, 2, 0), 14: (2, 2, 0), 15: (3, 2, 0), 16: (0, 3, 0), 17: (1, 3, 0), 18: (2, 3, 0), 19: (0, 4, 0), 20: (1, 4, 0), 21: (0, 5, 0), 22: (0, 0, 1), 23: (1, 0, 1), 24: (2, 0, 1), 25: (3, 0, 1), 26: (4, 0, 1), 27: (0, 1, 1), 28: (1, 1, 1), 29: (2, 1, 1), 30: (3, 1, 1), 31: (0, 2, 1), 32: (1, 2, 1), 33: (2, 2, 1), 34: (0, 3, 1), 35: (1, 3, 1), 36: (0, 4, 1), 37: (0, 0, 2), 38: (1, 0, 2), 39: (2, 0, 2), 40: (3, 0, 2), 41: (0, 1, 2), 42: (1, 1, 2), 43: (2, 1, 2), 44: (0, 2, 2), 45: (1, 2, 2), 46: (0, 3, 2), 47: (0, 0, 3), 48: (1, 0, 3), 49: (2, 0, 3), 50: (0, 1, 3), 51: (1, 1, 3), 52: (0, 2, 3), 53: (0, 0, 4), 54: (1, 0, 4), 55: (0, 1, 4), 56: (0, 0, 5)}
+        expected_cell_to_index = {(0, 0, 0): 1, (1, 0, 0): 2, (2, 0, 0): 3, (3, 0, 0): 4, (4, 0, 0): 5, (5, 0, 0): 6, (0, 1, 0): 7, (1, 1, 0): 8, (2, 1, 0): 9, (3, 1, 0): 10, (4, 1, 0): 11, (0, 2, 0): 12, (1, 2, 0): 13, (2, 2, 0): 14, (3, 2, 0): 15, (0, 3, 0): 16, (1, 3, 0): 17, (2, 3, 0): 18, (0, 4, 0): 19, (1, 4, 0): 20, (0, 5, 0): 21, (0, 0, 1): 22, (1, 0, 1): 23, (2, 0, 1): 24, (3, 0, 1): 25, (4, 0, 1): 26, (0, 1, 1): 27, (1, 1, 1): 28, (2, 1, 1): 29, (3, 1, 1): 30, (0, 2, 1): 31, (1, 2, 1): 32, (2, 2, 1): 33, (0, 3, 1): 34, (1, 3, 1): 35, (0, 4, 1): 36, (0, 0, 2): 37, (1, 0, 2): 38, (2, 0, 2): 39, (3, 0, 2): 40, (0, 1, 2): 41, (1, 1, 2): 42, (2, 1, 2): 43, (0, 2, 2): 44, (1, 2, 2): 45, (0, 3, 2): 46, (0, 0, 3): 47, (1, 0, 3): 48, (2, 0, 3): 49, (0, 1, 3): 50, (1, 1, 3): 51, (0, 2, 3): 52, (0, 0, 4): 53, (1, 0, 4): 54, (0, 1, 4): 55, (0, 0, 5): 56}
+        self.assertEqual(b.board, expected_board)
+        self.assertEqual(s.index_to_cell, expected_index_to_cell)
+        self.assertEqual(s.cell_to_index, expected_cell_to_index)
+        for key, value in s.index_to_cell.items():
+            self.assertTrue(value in s.cell_to_index.keys())
+            self.assertTrue(s.cell_to_index[value] in  s.index_to_cell.keys())
 
-#     def test_packing_matrix(self):
-#         s = solver.Solver()
-#         b = board.Board()
-#         pieces = []
-#         for p in piece.pieces:
-#             pieces.append(piece.Piece(p))
+    def test_packing_matrix(self):
+        s = solver.Solver()
+        b = board.Board()
+        pieces = []
+        for p in piece.pieces:
+            pieces.append(piece.Piece(p))
 
-#         s.initialise_packing_matrix(b, pieces)
-#         print(algorithm_x_functions.pretty_print(s.matrix))
+        s.initialise_packing_matrix(b, pieces)
+        self.assertEqual(s.matrix.num_cols, b.count_cells() + len(pieces))
+        self.assertEqual(s.matrix.num_rows, 4224)
 
-#     def test_packing_matrix_partial_config(self):
-#         s = solver.Solver()
-#         b = board.Board()
-#         pieces = []
-#         for p in piece.pieces:
-#             if p != 11:
-#                 pieces.append(piece.Piece(p))
+    def test_packing_matrix_partial_config(self):
+        s = solver.Solver()
+        b = board.Board()
+        pieces = []
+        for p in piece.pieces:
+            if p != 11:
+                pieces.append(piece.Piece(p))
 
-#         b.board[0][0][0] = 11
-#         b.board[0][1][0] = 11
-#         b.board[1][0][0] = 11
-#         b.board[1][1][0] = 11
+        b.board[0][0][0] = 11
+        b.board[0][1][0] = 11
+        b.board[1][0][0] = 11
+        b.board[1][1][0] = 11
 
-#         s.initialise_packing_matrix_partial_config(b, pieces)
-#         print(algorithm_x_functions.pretty_print(s.matrix))
+        s.initialise_packing_matrix_partial_config(b, pieces)
+        self.assertEqual(s.matrix.num_cols, b.count_cells() + len(pieces) + 1) # + 1 as piece 11 was never added to pieces[]
+        self.assertEqual(s.matrix.num_rows, 3375)
 
 
-#     def test_solve(self): 
-#         s = solver.Solver()
-#         b = board.Board()
-#         pieces = []
-#         for p in piece.pieces:
-#             pieces.append(piece.Piece(p))
+    def test_solve(self): 
+        s = solver.Solver()
+        b = board.Board()
+        pieces = []
+        for p in piece.pieces:
+            pieces.append(piece.Piece(p))
 
-#         rows = s.solve(pieces, b)
-#         print(s.rows_to_array_sol(rows, b))
+        rows = s.solve(pieces, b)
+        sol = s.rows_to_array_sol(rows, b)
+        self.assertTrue(sol)
+        b.board = sol
+        self.assertTrue(b.verify_board()) # use verify board to check all pieces are placed in valid placements
 
-#     def test_generate_solutions(self):
-#         s = solver.Solver()
-#         b = board.Board()
-#         pieces = []
-#         for p in piece.pieces:
-#             pieces.append(piece.Piece(p))
+    def test_generate_solutions(self):
+        s = solver.Solver()
+        b = board.Board()
+        pieces = []
+        for p in piece.pieces:
+            pieces.append(piece.Piece(p))
 
-#         count = 1
-#         for rows in s.generate_solutions(pieces, b):
-#             print(count)
-#             count += 1
+        count = 1
+        for rows in s.generate_solutions(pieces, b):
+            if count >= 10:
+                break
+            sol = s.rows_to_array_sol(rows, b)
+            self.assertTrue(sol)
+            b.board = sol
+            self.assertTrue(b.verify_board())  # use verify board to check all pieces are placed in valid placements
+            count += 1
+        self.assertEqual(count, 10)
+
