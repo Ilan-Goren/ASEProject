ee9c61656c96db7743490f65486bc7d4bd173921
cw523-01 <78119212+cw523-01@users.noreply.github.com>
2024-12-05 16:56:24 +0000

    implemented checking partial configuration on hex app

--- polyhex/polyhex.py
+++ polyhex/polyhex.py
@@ -40,14 +40,12 @@ class Polyhex_Solver:
         if self.board:
             if not b.verify_board():
                 b = board.Board()
-                print('not valid board')
                 return False
             b.board = self.board
 
         # Prepare pieces to place on the board
         pieces_to_place = []
         for p in piece.pieces:
-            print(self.pieces_placed)
             if p in self.pieces_placed:
                 continue
             pieces_to_place.append(piece.Piece(p))
@@ -57,5 +55,4 @@ class Polyhex_Solver:
         for rows in s.generate_solutions(pieces_to_place, b):
             solutions.append(s.rows_to_array_sol(rows, b))
             i+=1
-            print(i)
         return True
